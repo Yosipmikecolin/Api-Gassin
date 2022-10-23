6ca3ee13ff5a3bbf085856a8417481e49f3e84cc
Yosipadvantage <yosip.parrado@advantage.com.co>
2022-10-22 20:27:08 -0500

    multer agregado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+var path = require('path');
+const multer = require('multer')
 var cors = require('cors')
 const app = express();
 
 
+
 //VARIBALES DE ENTORNO
 app.set("port", process.env.PORT || 5000);
+
+
+//CONFIGURACION DE MULTER 
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, "storage"),
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+
+
+app.use(multer({
+    storage: storage,
+    dest: path.join(__dirname, "storage")
+}).single("avatar"))
+
 
 
 
@@ -13,8 +33,14 @@ app.use(express.json());
 app.use(cors());
 
 
+
+
+
+
+
 //ROUTES
-app.use("/destinos", require("./routes/destinos.routes"))
+app.use("/destinos", require("./routes/destinos.routes"));
+app.use("/upload", require("./routes/images.routes"));
 
 
 
---
+++ src/routes/images.routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+var path = require('path');
+
+
+
+
+
+router.post("/img", (req, res) => {
+    res.send(path.join("storage/" + req.file.originalname));
+    //console.log("Archivo", req.file)
+})
+
+
+module.exports = router;
