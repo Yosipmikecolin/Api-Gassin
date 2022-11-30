1a847faabd6c1b42c7a8ee98456b30d56ad18064
Yosipadvantage <yosip.parrado@advantage.com.co>
2022-11-30 18:45:22 -0500

    guias agregados

--- src/database/aventuras.js
+++ src/database/aventuras.js
@@ -4,38 +4,62 @@ const aventuras = [
         "url": "https://images.unsplash.com/photo-1665406857887-e53cfdbee0d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
         "destino": "Bosque encantado",
         "precio": 3200000,
-        "clima": "Calido"
+        "clima": "Calido",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+            "nombre": "Fred Guillermo"
+        }
     },
     {
         "id": 2,
         "url": "https://images.unsplash.com/photo-1568801556940-e5b3a55fa6ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
         "destino": "Desierto Woco",
         "precio": 5200000,
-        "clima": "Caliente"
+        "clima": "Caliente",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            "nombre": "Mike Pawer"
+        }
     }, {
         "id": 3,
         "url": "https://images.unsplash.com/photo-1495107334309-fcf20504a5ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         "destino": "Pradera de los henanos",
         "precio": 5400000,
-        "clima": "Frio"
+        "clima": "Frio",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            "nombre": "Sofia Andrea"
+        }
     }, {
         "id": 4,
         "url": "https://images.unsplash.com/photo-1482685945432-29a7abf2f466?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1189&q=80",
         "destino": "Cascadas de Belen",
         "precio": 4500000,
-        "clima": "Nieblado"
+        "clima": "Nieblado",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+            "nombre": "Andrea Salazar"
+        }
     }, {
         "id": 5,
         "url": "https://images.unsplash.com/photo-1534567153574-2b12153a87f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         "destino": "Zoologia Alfredo",
         "precio": 2500000,
-        "clima": "Caliente"
+        "clima": "Caliente",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=699&q=80",
+            "nombre": "Guillermo Toro"
+        }
     }, {
         "id": 6,
         "url": "https://images.unsplash.com/photo-1443827357341-5def682bb98b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
         "destino": "Jardin de Leden",
         "precio": 6400000,
-        "clima": "Calido"
+        "clima": "Calido",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+            "nombre": "Nicol Peña"
+        }
     }
 ]
 
--- src/database/vacaciones.js
+++ src/database/vacaciones.js
@@ -4,38 +4,62 @@ const vacaciones = [
         "url": "https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
         "destino": "Africa",
         "precio": 4500000,
-        "clima": "Caliente"
+        "clima": "Caliente",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+            "nombre": "Melisa Bolaños"
+        }
     },
     {
         "id": 2,
         "url": "https://images.unsplash.com/photo-1604433203862-93bc73b0f1e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
         "destino": "Qatar",
         "precio": 5600000,
-        "clima": "Caliente"
+        "clima": "Caliente",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+            "nombre": "Yosip Stiven"
+        }
     }, {
         "id": 3,
         "url": "https://images.unsplash.com/photo-1474181487882-5abf3f0ba6c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         "destino": "China",
         "precio": 3400000,
-        "clima": "Calido"
+        "clima": "Calido",
+        "guia": {
+            "avatar": "https://unsplash.com/es/fotos/cdksyTqEXzo",
+            "nombre": "Francisco Reyes"
+        }
     }, {
         "id": 4,
         "url": "https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
         "destino": "Francia",
         "precio": 3200000,
-        "clima": "Templado"
+        "clima": "Templado",
+        "guia": {
+            "avatar": "https://unsplash.com/es/fotos/7YVZYZeITc8",
+            "nombre": "Matias Cobasis"
+        }
     }, {
         "id": 5,
         "url": "https://images.unsplash.com/photo-1552832230-c0197dd311b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1096&q=80",
         "destino": "Roma",
         "precio": 4800000,
-        "clima": "Calido"
+        "clima": "Calido",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            "nombre": "Mike Colin"
+        }
     }, {
         "id": 6,
         "url": "https://images.unsplash.com/photo-1543832923-44667a44c804?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1044&q=80",
         "destino": "Inglaterra",
         "precio": 4300000,
-        "clima": "Neblado"
+        "clima": "Neblado",
+        "guia": {
+            "avatar": "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEyfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
+            "nombre": "Erika Juliana"
+        }
     }
 ]
 
